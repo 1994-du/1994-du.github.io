6eebcc0de1a49be5bb41d073e0a094e62bc34338
du_1994 <1746215067@qq.com>
2024-12-05 16:04:56 +0800

    2

---
+++ src/App.vue
@@ -0,0 +1,159 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="curriculum_vitae">
+    <div class="sticky_title">
+      <h4>个人信息</h4>
+    </div>
+    <div class="module">
+      <div class="info_item">
+        <span class="label">姓名：</span>
+        <span>杜欣欣</span>
+      </div>
+      <div class="info_item">
+        <span class="label">学历：</span>
+        <span>大专</span>
+      </div>
+      <div class="info_item">
+        <span class="label">专业：</span>
+        <span>计算机应用</span>
+      </div>
+      <div class="info_item">
+        <span class="label">联系方式：</span>
+        <span>17751216596</span>
+      </div>
+    </div>
+    <div class="sticky_title">
+      <h4>技能介绍</h4>
+    </div>
+    <div class="module">
+      <div class="skill_item">
+        1.精通HTML5、CSS3、JavaScript及新特性
+      </div>
+      <div class="skill_item">
+        2.熟练使用第三方UI框架：Echarts、Antv、Elementui、Vant、iView等
+      </div>
+      <div class="skill_item">
+        3.熟练使用Vue2/Vue3
+      </div>
+      <div class="skill_item">
+        4.熟练使用代码版本管理工具git和svn
+      </div>
+      <div class="skill_item">
+        5.可以快速解决🧍各个主流浏览器间的兼容性问题
+      </div>
+    </div>
+    <div class="sticky_title">
+      <h4>工作经历</h4>
+    </div>
+    <div class="module">
+      <div class="job_item">
+            <div class="job_header">
+                <div class="job_title">北京南天信息</div>
+                <div class="job_time">2023.02.27</div>
+                <span>-</span>
+                <div class="job_time">至今</div>
+            </div>
+            <div class="job_content">
+                <div class="job_content_item">
+                  1.主要负责中银金科及软件中心前端开发
+                </div>
+                <div class="job_content_item">
+                  2.技术栈：Vue、Element Ui、micro-app微前端框架
+                </div>
+                <div class="job_content_item">
+                  3.开发的项目有企架、COMS、COSS_JK、ccpsbnd征信、行家（移动端）等
+                </div>
+            </div>
+      </div>
+      <div class="job_item">
+          <div class="job_header">
+              <div class="job_title">北京中软国际科技服务有限公司</div>
+              <div class="job_time">2020.12</div>
+              <span>-</span>
+              <div class="job_time">2023.2</div>
+          </div>
+          <div class="job_content">
+              <div class="job_content_item">
+                  1.主要负责平安壹账通数据架构组前端开发并和产品充分沟通进行迭代开发
+              </div>
+              <div class="job_content_item">
+                  2.技术栈：Vue、ElementUi、Echarts、SpreadJs、Antv-g2、Antv-g6等
+              </div>
+              <div class="job_content_item">
+                  3.开发的项目有RMS、数据填报、数据融合、标签管理、数据流计算等
+              </div>
+          </div>
+      </div>
+      <div class="job_item">
+          <div class="job_header">
+              <div class="job_title">上海东方希杰商务有限公司</div>
+              <div class="job_time">2020.6</div>
+              <span>-</span>
+              <div class="job_time">2020.11</div>
+          </div>
+          <div class="job_content">
+              <div class="job_content_item">
+                  1.主要负责东方购物官网和MobileWeb中商品详情模板的开发
+              </div>
+              <div class="job_content_item">
+                  2.技术栈：Vue、Vant等
+              </div>
+              <div class="job_content_item">
+                  3.负责商品详情SKU、商品直播等模块及IOS和原生的页面嵌套工作
+              </div>
+          </div>
+      </div>
+      <div class="job_item">
+          <div class="job_header">
+              <div class="job_title">苏州采微互联科技有限公司</div>
+              <div class="job_time">2019.4</div>
+              <span>-</span>
+              <div class="job_time">2020.2</div>
+          </div>
+          <div class="job_content">
+              <div class="job_content_item">
+                  1.主要负责采微供应链系统中仓库管理和容器管理模块
+              </div>
+              <div class="job_content_item">
+                  2.技术栈：Vue、ElementUi、微信小程序等
+              </div>
+          </div>
+      </div>
+      <div class="job_item">
+          <div class="job_header">
+              <div class="job_title">河南超构网络科技有限公司</div>
+              <div class="job_time">2017.12</div>
+              <span>-</span>
+              <div class="job_time">2019.2</div>
+          </div>
+          <div class="job_content">
+              <div class="job_content_item">
+                  1.主要负责开发企业官网并参与后台管理系统的开发
+              </div>
+              <div class="job_content_item">
+                  2.技术栈：Vue、iView
+              </div>
+              <div class="job_content_item">
+                  3.负责商品详情SKU、商品直播等模块及IOS和原生的页面嵌套工作
+              </div>
+          </div>
+      </div>
+    </div>
+    <div class="sticky_title">
+      <h4>项目经验</h4>
+    </div>
+    <div class="module">
+      era中银香港项目--2023年10月底
+      综合经营服务管理COMS--2023年11月
+      COSS_JK
+      ccpsbnd--征信
+    </div>
+  </div>
+</template>
+
+<style lang="less" scoped>
+@import './styles/app.less';
+</style>
